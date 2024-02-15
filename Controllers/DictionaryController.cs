930d98f195d2f55cafecc7cf2b13ebe04d555c1f
samarpit sharma <SASharma@livegage.ai>
2024-02-15 18:21:07 +0530

    post loans using excel controller

--- Controllers/DictionaryController.cs
+++ Controllers/DictionaryController.cs
@@ -3,6 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using DD_Server.Model;
 using DD_Server.Persistence;
+using OfficeOpenXml;
+using Newtonsoft.Json;
 
 namespace DD_Server.Controllers2
 {
@@ -15,6 +17,64 @@ namespace DD_Server.Controllers2
         public DictionaryController(AppDbContext context)
         {
             _context = context;
+        }
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> Upload()
+        {
+            var file = Request.Form.Files[0];
+
+            if (file.Length > 0)
+            {
+                var filePath = Path.GetTempFileName();
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(stream);
+                }
+
+                // Process the file as needed, e.g., save to database
+                ConvertExcelToJson(filePath);
+
+
+
+                return Ok(new { message = "File uploaded successfully" });
+            }
+
+            return BadRequest("File not provided");
+        }
+        private void ConvertExcelToJson(string filePath)
+        {
+            var result = new List<Dictionary>();
+
+            using (var package = new ExcelPackage(new FileInfo(filePath)))
+            {
+                var worksheet = package.Workbook.Worksheets[0];
+
+                for (var rowNumber = 2; rowNumber <= worksheet.Dimension.End.Row; rowNumber++)
+                {
+                    var row = worksheet.Cells[rowNumber, 1, rowNumber, worksheet.Dimension.End.Column];
+
+                    var dataItem = new Dictionary
+                    {
+                        Container = row[rowNumber, 1].Text,
+                        DataPoint = row[rowNumber, 2].Text,
+                        DbColumnName = row[rowNumber, 3].Text,
+                        FieldType = row[rowNumber, 4].Text,
+                        DbDataType = row[rowNumber, 5].Text,
+                        Definition = row[rowNumber, 6].Text,
+                        PossibleValues = row[rowNumber, 7].Text.Split(',').Select(value => value.Trim()).ToArray(),
+                        Synonyms = row[rowNumber, 8].Text.Split(',').Select(value => value.Trim()).ToArray(),
+                        CalculatedInfo = row[rowNumber, 9].Text,
+                        // IsLocked = false
+                    };
+
+                    result.Add(dataItem);
+                }
+            }
+            _context.Dictionary.AddRange(result);
+            _context.SaveChanges();
+            // return result;
         }
 
         // GET: api/Dictionary
